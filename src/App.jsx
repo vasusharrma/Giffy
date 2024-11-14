41c197a5e5b6c5bde7f6e63c01bcd26aaa71da8f
vasusharrma <sharmavashu099@gmail.com>
2024-11-14 19:23:16 +0530

    just getting started

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+    return (
+        <>
+            <div>
+                <h1>
+                    Jai Shree Ram
+                </h1>
+            </div>
+        </>
+    )
+}
+
+export default App
